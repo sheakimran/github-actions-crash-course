db126f9301acc219e2f5c36a7287c028cecc8f85
sheakimran <imran.antor2010@gmail.com>
2024-05-30 22:46:52 +0600

    hhg

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           {process.env.REACT_APP_WELCOME_TEXT || "Github Actions Tutorial"} - By
-          sheakdev
+          Saidur
         </p>
         <a
           className="App-link"
